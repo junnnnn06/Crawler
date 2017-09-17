f358f6952fddfd25ded4b69a9fae0af3d9cfa5a8
junnnnn06 <junnnnn06@icloud.com>
2017-09-17 16:36:05 +0900

    info of yahoo

--- scraping_html.rb
+++ scraping_html.rb
@@ -1,15 +1,13 @@
-# URLにアクセスするためのライブラリの読み込み
 require 'open-uri'
-# Nokogiriライブラリの読み込み
-require 'nokogiri'
-# スクレイピング先のURL
+
+#スクレイピング先のURL
 url = 'https://www.yahoo.co.jp/'
+#user_agentの偽装
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 charset = nil
-html = open(url) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
+html = open(url, "User-Agent" => user_agent) do |f|
+  charset = f.charset #文字種別を取得
+  f.read
 end
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-# タイトルを表示
-p doc.title
+
+puts html
